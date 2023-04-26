f58483d181867426f5e476071e0db6b309e52a45
AdamMescher <admescher@gmail.com>
2023-04-26 02:21:59 -0600

    1 file modified

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,22 +1,5 @@
-import type { Preview, StoryObj } from '@storybook/react';
-
-import * as NextImage from 'next/image';
-
+import type { Preview } from '@storybook/react';
 import '../src/app/styles/globals.scss';
-
-const OriginalNextImage = NextImage.default;
-
-Object.defineProperty(NextImage, 'default', {
-  configurable: true,
-  value: (props) => (
-    <OriginalNextImage
-      {...props}
-      unoptimized
-      // this is new!
-      blurDataURL="data:image/jpeg;base64,/9j/2wBDAAYEBQYFBAYGBQYHBwYIChAKCgkJChQODwwQFxQYGBcUFhYaHSUfGhsjHBYWICwgIyYnKSopGR8tMC0oMCUoKSj/2wBDAQcHBwoIChMKChMoGhYaKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCgoKCj/wAARCAADAAQDASIAAhEBAxEB/8QAFQABAQAAAAAAAAAAAAAAAAAAAAf/xAAbEAADAAMBAQAAAAAAAAAAAAABAgMABAURUf/EABUBAQEAAAAAAAAAAAAAAAAAAAMF/8QAFxEAAwEAAAAAAAAAAAAAAAAAAAECEf/aAAwDAQACEQMRAD8Anz9voy1dCI2mectSE5ioFCqia+KCwJ8HzGMZPqJb1oPEf//Z"
-    />
-  ),
-});
 
 const preview: Preview = {
   parameters: {
